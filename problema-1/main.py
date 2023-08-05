9b20ef005e8855723b1b8c10a157f9b7f0dbefb9
igordbauer <igordbauer@gmail.com>
2023-08-04 23:01:12 -0300

    testing-values-and-validation

--- problema-1/main.py
+++ problema-1/main.py
@@ -18,3 +18,30 @@ class MonetaryValue:
         value = f"{self.valorFloat():_.2f}"
         finalNumber = f"{coin_type} {formatNumber(value,separator,counter)}"
         return finalNumber
+
+
+int_value = input("Insira um valor:")
+coin_type_isValid = False
+
+while coin_type_isValid == False:
+    coin_type = str(input("Insira uma unidade monetária: [R$ ou $]: "))
+    if coin_type == "R$" or coin_type == "$":
+        coin_type_isValid = True
+    else:
+        print("Insira uma unidade monetária válida")
+
+separator_isValid = False
+while separator_isValid == False:
+    separator = str(input("Insira um separador: "))
+    if separator == "," or separator == ".":
+        separator_isValid = True
+    else:
+        print("Insira um separador válido")
+
+
+object = MonetaryValue(int_value, coin_type, separator)
+
+print(object.finalValue("R$", "."))
+print(object.finalValue("R$", ","))
+print(object.finalValue("$", "."))
+print(object.finalValue("$", ","))
