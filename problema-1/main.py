93f4d28dad285adcb3e1a93c5b8c09cbf18e5b88
igordbauer <igordbauer@gmail.com>
2023-08-04 23:00:27 -0300

    class-body-creation-and-methods

--- problema-1/main.py
+++ problema-1/main.py
@@ -1,1 +1,20 @@
-print('hello')
+def formatNumber(value, separator, counter):
+    return value.replace(f"{counter}", separator).replace("_", f"{counter}")
+
+
+class MonetaryValue:
+    def __init__(self, int_value, coin_type, separator):  # constructor
+        self.int_value = int_value
+        self.coin_type = coin_type
+        self.separator = separator[0]
+
+    def valorFloat(self):
+        return float(self.int_value)
+
+    def finalValue(self, coin_type, separator):
+        counter = ","
+        if separator == ",":
+            counter = "."
+        value = f"{self.valorFloat():_.2f}"
+        finalNumber = f"{coin_type} {formatNumber(value,separator,counter)}"
+        return finalNumber
